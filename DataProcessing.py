ef46954457cfb6aab31a02b2fb55f51b550dda99
Shahinul Hoque <shoque@vols.utk.edu>
2022-11-28 20:51:07 -0500

    Binary classification for Dog vs Cat working

--- DataProcessing.py
+++ DataProcessing.py
@@ -2,26 +2,38 @@ import numpy as np
 import tensorflow as tf
 from Nodes import Node
 import keras.utils.np_utils as kutils
+from keras.preprocessing.image import ImageDataGenerator
 
 
 def getNodes():
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
 
-    np.random.shuffle(x_train)
-    np.random.shuffle(y_train)
-    np.random.shuffle(x_test)
-    np.random.shuffle(y_test)
+    train_generator = train_datagen.flow_from_directory(
+        'data/train',
+        target_size=(32, 32),
+        batch_size=20000,
+        class_mode='binary')
 
-    x_train = x_train/255.0
-    x_test = x_test/255.0
-    y_train = kutils.to_categorical(y_train, 10)
-    y_test = kutils.to_categorical(y_test, 10)
+
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    test_generator = test_datagen.flow_from_directory(
+            'data/test',
+            target_size=(32, 32),
+            batch_size=2500,
+            class_mode='binary')
+
+    (x_train, y_train) = train_generator.next()
+    (x_test, y_test) = test_generator.next()
 
     nodes = []
 
     for i in range(100):
-        start = 500*i
-        end = 500*(i+1)
+        start = 200*i
+        end = 200*(i+1)
         n = Node(x_train[start:end], y_train[start:end], node_number=i)
         nodes.append(n)
 
@@ -29,6 +41,6 @@ def getNodes():
 
 def makeNodeMalicious(node_obj):
     x_train, y_train = node_obj.get_data()
-    y_train_rand = np.random.randint(10, size=len(y_train))
-    y_train_rand = kutils.to_categorical(y_train_rand, 10)
+    y_train_rand = np.random.randint(2, size=len(y_train))
+
     node_obj.set_data(x_train, y_train_rand)
--- Model.py
+++ Model.py
@@ -2,42 +2,62 @@
 import tensorflow as tf
 from keras import Sequential
 from keras.optimizers import SGD
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D,BatchNormalization,Dropout
 
 
 def getModel(model_name):
     model = Sequential()
-    # feature_extractor = None
+    keras_model = None
 
-    # if model_name == "ResNet50":
-    #     feature_extractor = tf.keras.applications.ResNet50(include_top = False, weights = 'imagenet', input_shape = (32,32,3), classes = 10)
+    if model_name == "ResNet50":
+        keras_model = tf.keras.applications.ResNet50(include_top = False, weights = 'imagenet', input_shape = (32,32,3))
         
-    # if model_name == "DenseNet121":
-    #     feature_extractor = tf.keras.applications.DenseNet121(include_top = False, weights = 'imagenet', input_shape = (32,32,3), classes = 10)
+    if model_name == "DenseNet121":
+        keras_model = tf.keras.applications.DenseNet121(include_top = False, weights = 'imagenet', input_shape = (32,32,3))
 
-    # if model_name == "MobileNet":
-    #     feature_extractor = tf.keras.applications.MobileNet(include_top = False, weights = 'imagenet', input_shape = (32,32,3), classes = 10)
+    if model_name == "MobileNet":
+        keras_model = tf.keras.applications.MobileNet(include_top = False, weights = 'imagenet', input_shape = (32,32,3))
 
-    # if model_name == "VGG16":
-    #     feature_extractor = tf.keras.applications.VGG16(include_top = False, weights = 'imagenet', input_shape = (32,32,3), classes = 10)
+    if model_name == "VGG16":
+        keras_model = tf.keras.applications.VGG16(include_top = False, weights = 'imagenet', input_shape = (32,32,3))
 
-    # if model_name == "InceptionV3":
-    #     feature_extractor = tf.keras.applications.InceptionV3(include_top = False, weights = 'imagenet', input_shape = (32,32,3), classes = 10)
+    if model_name == "InceptionV3":
+        keras_model = tf.keras.applications.InceptionV3(include_top = False, weights = 'imagenet', input_shape = (32,32,3))
 
-    # if model_name == "EfficientNetB7":
-    #     feature_extractor = tf.keras.applications.EfficientNetB7(include_top = False, weights = 'imagenet', input_shape = (32,32,3), classes = 10)
+    if model_name == "EfficientNetB7":
+        keras_model = tf.keras.applications.EfficientNetB7(include_top = False, weights = 'imagenet', input_shape = (32,32,3))
     
+    if model_name == "Generic":
+        model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32,32,3)))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
 
-    # model.add(feature_extractor)
-    # model.add(Flatten())
-    # model.add(Dense(10, activation='softmax'))
-    # optimizer = SGD(learning_rate=0.001, momentum=0.9, decay=0.0, nesterov=True)
-    # model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    model.add(Conv2D(32,3,3,input_shape=(32,32,3),activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Flatten())
-    model.add(Dense(output_dim=128,activation='relu'))
-    model.add(Dense(output_dim=1,activation='sigmoid'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
 
+        model.add(Conv2D(128, (3, 3), activation='relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(512, activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        model.add(Dense(1,activation='sigmoid'))
+        sgd = SGD(lr=0.001, momentum=0.9)
+        model.compile(optimizer=sgd, loss='binary_crossentropy',  metrics=['accuracy'])
+        return model
+
+    out=keras_model.layers[-1].output
+    out = Flatten()(out)
+    output = Dense(1, activation='sigmoid')(out)
+    model = tf.keras.models.Model(inputs=keras_model.input, outputs=output)
+    sgd = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=sgd, loss='binary_crossentropy',  metrics=['accuracy'])
 
     return model
